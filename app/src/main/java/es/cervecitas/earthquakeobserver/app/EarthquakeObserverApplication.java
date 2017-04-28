63feef6db897cae9d7317f9494bcde5d61e5eee5
Luis <luismu78@gmail.com>
2017-04-28 12:15:28 +0200

    internet permission Application class

---
+++ app/src/main/java/es/cervecitas/earthquakeobserver/app/EarthquakeObserverApplication.java
@@ -0,0 +1,15 @@
+package es.cervecitas.earthquakeobserver.app;
+
+import android.app.Application;
+
+/**
+ * Created by luism on 28/04/2017.
+ */
+
+public class EarthquakeObserverApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
